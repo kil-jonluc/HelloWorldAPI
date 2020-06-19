9a6290230315ef38dbcd51f4d7bf526d90e201a1
Jonathan Kilpatrick <kilpatrick.jonluc@outlook.com>
2020-06-19 00:20:08 -0400

    add route and start server

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,6 +1,7 @@
 import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
-
 import scala.concurrent.ExecutionContext
 
 object Server extends App {
@@ -10,8 +11,13 @@ object Server extends App {
   //used to manage actors. It is used for creating and looking them up
   implicit val system: ActorSystem = ActorSystem("helloworld");
   //in charge of executing Futures
-  implicit val executor: ExecutionContext = system.dispatcherimplicit;
+  implicit val executor: ExecutionContext = system.dispatcher
   // in charge of running streams
-  val materializer: ActorMaterializer = ActorMaterializer();
+  implicit val materializer: ActorMaterializer = ActorMaterializer();
 
+  //Route
+  def route = path("hello") {  get {    complete("Hello, World!")  }};
+
+  //Start the server
+  Http().bindAndHandle(route, host, port);
 }
