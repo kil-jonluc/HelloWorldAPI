59c0f59b6a8b77885d3ef68e0915f04a9d2cf970
Jonathan Kilpatrick <kilpatrick.jonluc@outlook.com>
2020-06-19 00:01:42 -0400

    instantiated dependencies

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,3 +1,17 @@
-object Server {
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+
+import scala.concurrent.ExecutionContext
+
+object Server extends App {
+
+  val host = "0.0.0.0"
+  val port = 9000;
+  //used to manage actors. It is used for creating and looking them up
+  implicit val system: ActorSystem = ActorSystem("helloworld");
+  //in charge of executing Futures
+  implicit val executor: ExecutionContext = system.dispatcherimplicit;
+  // in charge of running streams
+  val materializer: ActorMaterializer = ActorMaterializer();
 
 }
